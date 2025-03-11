02f5c3b0897c5ea474faa3e4cf91d8c7db4aa9f5
tuan <78714721+Chaiyanan8308@users.noreply.github.com>
2025-03-11 17:00:17 +0700

    Test #2

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const dbUrl = 'postgres://webadmin:IYOhba51267@node76611-jsproject.proen.app.ruk-com.cloud:11620/ReportIssue';
+const dbUrl = 'postgres://webadmin:IYOhba51267@node76611-jsproject.proen.app.ruk-com.cloud:5432/ReportIssue';
 const sequelize = new Sequelize(dbUrl);
 sequelize.authenticate()
     .then(() => console.log("Database connected..."))
