b0bd0cb3e4965ed2f9b8c2830b187554f26de885
tuan <78714721+Chaiyanan8308@users.noreply.github.com>
2025-03-12 19:48:01 +0700

    #

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 //const dbUrl = 'postgres://webadmin:IYOhba51267@node76611-jsproject.proen.app.ruk-com.cloud:11620/ReportIssue';
 //const dbUrl = 'postgres://webadmin:KYMhge41918@node77103-jsproject.proen.app.ruk-com.cloud:11850/ReportIssue'
-const dbUrl = 'postgres://webadmin:PMEpce71941@node77144-jsproject.proen.app.ruk-com.cloud:11796/ReportIssue'
+const dbUrl = 'postgres://webadmin:PMEpce71941@10.104.13.224:5432/ReportIssue'
 //const dbUrl = 'postgres://webadmin:KYMhge41918@10.104.6.80:11850/ReportIssue'
 const sequelize = new Sequelize(dbUrl);
 sequelize.authenticate()
